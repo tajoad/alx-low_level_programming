1a88a0fc90cf2b4df6241a605c4d6c32a96c85cf
tajoad <tajoad@gmail.com>
2022-06-30 14:27:04 -0700

    new push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
 * main -prints the size of various types
 * Return: 0 if exited properly, non-zero otherwise
 */
-int main (void)
+int main(void)
 {
-puts ("\"programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
