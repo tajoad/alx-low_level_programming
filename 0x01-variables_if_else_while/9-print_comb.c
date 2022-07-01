502e1e08dcbb5f4754e36d1727b1fc5fb280ca59
tajoad <tajoad@gmail.com>
2022-07-01 15:24:48 -0700

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,24 +3,25 @@
 #include <time.h>
 
 /**
-* main - Entry point
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-int d;
+  int d;
 
-for (d = '0'; d <= '9'; d++)
-{
-putchar(d);
-if (d != '9')
-{
-putchar(',');		
-putchar(' ');	
-}
-}
-putchar('\n');
+  for (d = '0'; d <= '9'; d++)
+    {
+      putchar(d);
+      if (d != '9')
+	{
+	  putchar(',');
+	  putchar(' ');
+	}
+    }
+  putchar('\n');
 
-return (0);
+  return (0);
 }
