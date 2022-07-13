11ae8c94f2b28ad182c6255f729b5f8d6d225d21
tajoad <tajoad@gmail.com>
2022-07-13 13:18:08 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int x, sum, y;
